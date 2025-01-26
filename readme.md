aa98dc70b6bee84c0438aba6b448a0e4325cdc2a
nik1tosssa <realprimestels@gmail.com>
2025-01-26 16:16:03 +0300

    rework as djang project

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-first commit to check
+структура
+repo/project_name/app_name/templates/app_name/ - folder for .html
+repo/project_name/app_name/static/app_name/ - folder for static of app(.css & img & .js ...)
---
+++ school_rating/main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'main/index.html')
+
+def about(request):
+    return render(request,'main/about.html')
+
+def courses(request):
+    return render(request,'main/courses.html')
